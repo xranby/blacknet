82bc74dd50fef2efae2d5dc34d82f45ca480195c
Xerxes Rånby <xerxes@gudinna.com>
2019-01-03 09:18:22 +0100

    Connection: process received packets from one connection in parallel

--- src/main/kotlin/ninja/blacknet/network/Connection.kt
+++ src/main/kotlin/ninja/blacknet/network/Connection.kt
@@ -71,7 +71,10 @@ class Connection(
                     dos("deserialization failed")
                     continue
                 }
-                packet.process(this)
+                val connection = this
+                launch {
+                    packet.process(connection)
+                }
             }
         } catch (e: ClosedReceiveChannelException) {
         } catch (e: CancellationException) {
